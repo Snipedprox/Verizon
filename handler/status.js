ddef20b975aa2087914a1a610bcf5ce3d69731f5
Lexxy <85262576+Snipedprox@users.noreply.github.com>
2021-12-14 06:01:19 -0300

    Add files via upload

---
+++ handler/status.js
@@ -0,0 +1,13 @@
+module.exports = (bot) => {
+    bot.status({
+        text: "@0xF and my server prefix is :",
+        type: "PLAYING",
+        time: 12
+    })
+
+    bot.status({
+        text: "I dont know about you but i love this server!",
+        type: "PLAYING",
+        time: 12
+    })
+}
