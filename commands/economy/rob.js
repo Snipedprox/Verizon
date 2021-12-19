85c3bd0031a6ff8fadf75383c53490d51cabb16e
Lexxy <85262576+Snipedprox@users.noreply.github.com>
2021-12-19 12:26:36 -0300

    Update rob.js

--- commands/economy/rob.js
+++ commands/economy/rob.js
@@ -7,7 +7,7 @@ $color[1;$getVar[color]]
 $thumbnail[1;$userAvatar[$clientid]]
 $title[1;$username stole from $username[$mentioned[1]]]
 $description[1;
-\` you stole a total of $random[0;2500]\`$getVar[symbol]\` from $username[$mentioned[1]]\`]
+\` you stole a total of $random[0;2500]\`$getVar[symbol] from $username[$mentioned[1]]\`]
 $globalCooldown[60s;You can rob someone again in %time%]
 $onlyIf[$getGlobalUserVar[cash;$authorID]>=2500;\`Your wallet needs to contain at least 2500$getvar[symbol] to rob someone.\`]
 $onlyIf[$getGlobalUserVar[cash;$mentioned[1]]>=2500;\`Their wallet needs to contain at least 2500$getvar[symbol]\`]
